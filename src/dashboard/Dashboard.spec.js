ad7bb719c5d9b8e978df540f0d0ed1d3bd9f0ba2
linkel <ironcarbide@gmail.com>
2019-03-06 13:14:13 -0800

    Added basic tests.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
-// Test away
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import Dashboard from './Dashboard.js';
+
+describe('<Dashboard />', () => {
+    it('should render without crashing', () => {
+        const { getByText } = render(<Dashboard/>);
+    });
+  });
+  
